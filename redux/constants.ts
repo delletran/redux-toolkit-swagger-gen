f4b2acd5ff1ab60cd529e71ea6707dd0d8ea73c8
Rodel Letran <delio.letran@gmail.com>
2025-03-15 11:54:10 +0800

    Refactor constants to use single quotes

--- redux/constants.ts
+++ redux/constants.ts
@@ -1,47 +1,48 @@
 // default form slices
 export const DEFAULT_FORM_SLICES = {
-  AUTH: "auth-slice",
-  LOGIN: "login-form-slice",
-  RESET_PASSWORD: "reset-password-form-slice",
-  ACCOUNT_RECOVERY: "account-recovery-form-slice",
+  AUTH: 'auth-slice',
+  LOGIN: 'login-form-slice',
+  RESET_PASSWORD: 'reset-password-form-slice',
+  ACCOUNT_RECOVERY: 'account-recovery-form-slice',
 } as const
 
 // #region Form Slice
-export const FORM_SLICE = {} as const
+export const FORM_SLICE = {
+} as const
 // #endregion
 
 // #region Thunk Slice
 export const ASYNC_THUNK_SLICE = {
-  LOGIN: "login-thunk-slice",
+  LOGIN: 'login-thunk-slice'
 } as const
 // #endregion
 
-export const SKIP_KEYS = ["created_at", "updated_at", "deleted_at"]
+export const SKIP_KEYS = ['created_at', 'updated_at', 'deleted_at']
 
 const REDUX_SLICE = {
-  SAMPLE: "sample-slice",
+  SAMPLE: 'sample-slice',
   ...FORM_SLICE,
-  ...DEFAULT_FORM_SLICES,
+  ...DEFAULT_FORM_SLICES
 } as const
 
 const REDUX_API = {
-  SAMPLE: "sample-api",
-  USER: "user-api",
-  AUTH_GROUP: "auth-group-api",
-  BRANCH: "branch-api",
+  SAMPLE: 'sample-api',
+  USER: 'user-api',
+  AUTH_GROUP: 'auth-group-api',
+  BRANCH: 'branch-api',
   PARAMS: {
     ALL: {
-      SIZE: 1000,
-    },
-  },
+      SIZE: 1000
+    }
+  }
 } as const
 
 export const REDUX = {
   FIELD: {
-    KEY: "_latestKey",
-    SKIPS: SKIP_KEYS,
+    KEY: '_latestKey',
+    SKIPS: SKIP_KEYS
   },
   SLICE: REDUX_SLICE,
   THUNK: ASYNC_THUNK_SLICE,
-  API: REDUX_API,
+  API: REDUX_API
 } as const
